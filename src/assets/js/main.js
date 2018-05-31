9dcf59902b0dcb43d1f10d666e259dbd5acdae4e
Daniel Stephany <Daniel@Daniels-MacBook-Pro-2.local>
2018-05-30 17:06:49 -0700

    made restaurant cards into responsive grid

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -138,27 +138,34 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
 
-  const image = document.createElement('img');
-  image.className = 'restaurant-img';
-  image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
+  const imgContainer = document.createElement("div");
+  imgContainer.className = "image-container"
+  imgContainer.style.backgroundImage = "url("+DBHelper.imageUrlForRestaurant(restaurant) +")";
+  imgContainer.setAttribute("role", "img");
+  imgContainer.setAttribute("aria-label", restaurant.name);
+  li.append(imgContainer);
 
-  const name = document.createElement('h1');
+  const details = document.createElement("div");
+  details.className = "details"
+
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  details.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  details.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  details.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  more.setAttribute("aria-label", "view details for " + restaurant.name);
+  details.append(more)
+  li.append(details);
 
   return li
 }
